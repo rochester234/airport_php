43c07d4f0a48936eb915d713a4edd8b40e27b9e8
rochester234 <beccollins82@hotmail.com>
2015-11-17 08:45:44 +0000

    initial commit - phpunit install and test

---
+++ htdocs/stats/Test/FirstTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace stats\Test;
+
+class FirstTest extends \PHPUnit_Framework_TestCase
+{
+  public function testUselessness()
+  {
+      $this->assertTrue(true);
+  }
+}
---
+++ htdocs/stats/Test/airportTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace stats\Test;
+
+use stats\Airport;
+
+class airportTest extends \PHPUnit_Framework_TestCase
+{
+  public function testUselessness()
+  {
+      $this->assertTrue(true);
+  }
+}
